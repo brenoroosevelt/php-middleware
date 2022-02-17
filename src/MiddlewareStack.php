8baf2a2d4fd7100ffdfef3fc86ed5ece0d69b4f5
Breno Roosevelt <breno.jesus@ufms.br>
2022-02-17 17:05:10 -0400

    Initial commit

--- src/MiddlewareStack.php
+++ src/MiddlewareStack.php
@@ -4,23 +4,37 @@ declare(strict_types=1);
 namespace BrenoRoosevelt\Middleware;
 
 use BrenoRoosevelt\Middleware\Middlewares\CallableMiddleware;
+use BrenoRoosevelt\Middleware\Middlewares\ErrorMiddleware;
 use BrenoRoosevelt\Middleware\Middlewares\LazyMiddleware;
 use BrenoRoosevelt\Middleware\Middlewares\DummyMiddleware;
 use BrenoRoosevelt\Psr11\NullContainer;
 use Psr\Container\ContainerInterface;
+use RuntimeException;
 
 class MiddlewareStack implements MiddlewareInterface
 {
     /** @var MiddlewareInterface[] */
     private array $middlewares;
     private ContainerInterface $container;
+    private MiddlewareInterface $stackExhaustedStrategy;
 
     final public function __construct(
         array $middlewares,
-        ?ContainerInterface $container = null
+        ?ContainerInterface $container = null,
+        ?MiddlewareInterface $stackExhaustedStrategy = null
     ) {
         $this->container = $container ?? new NullContainer;
         $this->middlewares = array_map(fn($m) => self::toMiddleware($m, $this->container), $middlewares);
+        $this->stackExhaustedStrategy =
+            $stackExhaustedStrategy ??
+            new ErrorMiddleware(
+                new RuntimeException('Middleware stack exhausted, missing final response middleware')
+            );
+    }
+
+    public function setStackExhaustedStrategy(MiddlewareInterface $middleware)
+    {
+        $this->stackExhaustedStrategy = $middleware;
     }
 
     public function prepend(MiddlewareInterface|callable|string $middleware): self
@@ -46,8 +60,9 @@ class MiddlewareStack implements MiddlewareInterface
     public function __invoke($subject)
     {
         $stack = $this->middlewares;
-        $middleware = $stack[0] ?? new DummyMiddleware;
+        $middleware = $stack[0] ?? $this->stackExhaustedStrategy;
         array_shift($stack);
+
         return $middleware->process($subject, new self($stack, $this->container));
     }
 
